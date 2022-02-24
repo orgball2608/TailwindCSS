0667a6a7c980a5fbc3d8e7fef8f4749c523a6fc4
orgball2608 <81631701+orgball2608@users.noreply.github.com>
2022-02-24 19:57:04 +0700

    m

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./*.html"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        35: "35px",
+      },
+      colors: {
+        gray33: "#333",
+        orangefa: "#ffa400",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["odd"],
+    },
+  },
+  plugins: [],
+};
